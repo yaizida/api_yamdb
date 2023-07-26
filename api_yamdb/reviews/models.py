e2b426b18d10b3741129e482553122176ef04b25
izida <109024816+yaizida@users.noreply.github.com>
2023-07-26 16:54:30 +0300

    Update models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 ROLES_CHOICES = (
-    ('admin', 'Пользователь'),
+    ('user', 'Пользователь'),
     ('moderator', 'Модератор'),
     ('admin', 'Администратор'),
 )
