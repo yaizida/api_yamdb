5d176a642a556deb790329be2a2785393d187247
Никита Бурковский <myizizda@gmail.com>
2023-07-26 12:43:28 +0300

    Создал кастомную модель User

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,60 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+ROLES_CHOICES = (
+    ('admin', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
+)
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        unique=True,
+        max_length=150,
+    )
+    email = models.EmailField(
+        unique=True,
+        max_length=150,
+    )
+    role = models.CharField(
+        choices=ROLES_CHOICES,
+        default=settings.DEFAULT_USER,
+        max_length=max(len(role) for role, _ in ROLES_CHOICES)
+    )
+    bio = models.TextField(blank=True, null=True)
+    confirmation_code = models.CharField(
+        max_length=10,
+        blank=True,
+        null=True
+    )
+    first_name = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_moderator(self):
+        pass
+
+    @property
+    def is_admin(self):
+        pass
 
 
 class Category(models.Model):
