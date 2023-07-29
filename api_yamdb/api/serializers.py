c026363b88d0865d1bef24cc7ba71b12a414733d
Александр Лаврушенков <lavralex2015@yandex.ru>
2023-07-29 20:00:12 +0300

    вивки для произведений, категорий и жанров

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+
+from reviews.models import Category, Genre, Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('name', 'slug')
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+
+
+class TitleGetSerializer(serializers.ModelSerializer):
+    "Сериализатор для GET запроса"
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(
+        read_only=True,
+        many=True
+    )
+    rating = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Title
+
+
+class TitleEditSerializer(serializers.ModelSerializer):
+    "Сериализатор для ввода, изменения и удаления данных"
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all()
+    )
+    genre = serializers.SlugRelatedField(
+        many=True,
+        slug_field='slug',
+        queryset=Genre.objects.all()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Title
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets, filters
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from reviews.models import Category, Genre, Title
+from .serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleGetSerializer,
+    TitleEditSerializer,
+)
+
+
+class CategoryGenreMixinSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', )
+    lookup_field = 'slug'
+
+
+class CategoryViewSet(CategoryGenreMixinSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(CategoryGenreMixinSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return TitleGetSerializer
+        return TitleEditSerializer
