a71f5571ab06de8662a96a9c06820b5321bc1305
Антон К <1329474@gmail.com>
2023-07-30 21:27:01 +0500

    Добавил функцию подсчета средней оценки(рейтинга) для произведения на основе оценок пользователя

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
+from django.db.models import Avg
 
 
 from reviews.models import Category, Genre, Title, Review, Comment
@@ -58,7 +59,9 @@ class ReviewViewSet(BaseViewSet):
 
     def get_queryset(self):
         title_id = self.kwargs.get('title_id')
-        return Review.objects.filter(title_id=title_id)
+        return Review.objects.filter(title_id=title_id).annotate(
+            rating=Avg('score')
+        )
 
     def perform_create(self, serializer):
         title_id = self.kwargs.get('title_id')
@@ -84,4 +87,3 @@ class CommentViewSet(BaseViewSet):
         review_id = self.kwargs.get('review_id')
         review = get_object_or_404(Review, id=review_id)
         serializer.save(author=self.request.user, review=review)
-
