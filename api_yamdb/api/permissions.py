265d8aa3473f48b1a4ad1f8ecbdfa919ea2555b7
izida <109024816+yaizida@users.noreply.github.com>
2023-08-10 12:28:06 +0300

    Update permissions.py
    Вот теперь то что нужно

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,7 +8,7 @@ class AdminOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_superuser
+        return request.user.is_authenticated and request.user.is_admin
 
 
 class IsAuthorOrAdminOrModerator(permissions.BasePermission):
